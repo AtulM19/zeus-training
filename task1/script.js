3e412defdcb321c1e9fcd4e0248fbb574e0c6046
AtulM19 <atul.mishra@zeuslearning.com>
2023-07-12 16:20:22 +0530

    updates

--- task1/script.js
+++ task1/script.js
@@ -6,6 +6,8 @@ const fullname = document.getElementById('fullname');
 const comments = document.getElementById('comments');
 const male = document.getElementById('male');
 const female = document.getElementById('female');
-
+const add = function (a, b) {
+    return a + b    
+}
 form.addEventListener('submit', (e) => {
  
